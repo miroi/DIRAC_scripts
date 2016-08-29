69cfc7128c15b9c6cf13ca4588fb18a77ce176e4
Miro ILIAS <Miroslav.Ilias@umb.sk>
2016-08-29 19:02:39 +0200

    upd

--- local_runs/umb_sk/grid_umb_sk/cfour/simple_test/pbs_runjob.sh
+++ local_runs/umb_sk/grid_umb_sk/cfour/simple_test/pbs_runjob.sh
@@ -15,8 +15,39 @@
 #PBS -j oe
 #PBS -q batch
 
+echo "Working host is: "; hostname -f
+source /mnt/apps/intel/bin/compilervars.sh intel64
+source /mnt/apps/pgi/environment.sh
+
+# libnumma for PGI
+export LD_LIBRARY_PATH=/home/milias/bin/lib64_libnuma:$LD_LIBRARY_PATH
+
+echo "My PATH=$PATH"
+echo "Running on host `hostname`"
+echo "Time is `date`"
+echo "Directory is `pwd`"
+echo "This jobs runs on the following processors:"
+echo `cat $PBS_NODEFILE`
+# Extract number of processors
+NPROCS_PBS=`wc -l < $PBS_NODEFILE`
+NPROCS=`cat /proc/cpuinfo | grep processor | wc -l`
+echo "This node has $NPROCS CPUs."
+echo "This node has $NPROCS_PBS CPUs allocated for PBS calculations."
+echo "PBS_SERVER=$PBS_SERVER"
+echo "PBS_NODEFILE=$PBS_NODEFILE"
+echo "PBS_O_QUEUE=$PBS_O_QUEUE"
+echo "PBS_O_WORKDIR=$PBS_O_WORKDIR"
+# ...
+export MKL_NUM_THREADS=$NPROCS_PBS
+echo "MKL_NUM_THREADS=$MKL_NUM_THREADS"
+#export MKL_DOMAIN_NUM_THREADS="MKL_BLAS=$NPROCS"
+export OMP_NUM_THREADS=1
+export MKL_DYNAMIC="FALSE"
+export OMP_DYNAMIC="FALSE"
+
+
 #CFOUR=/scrcluster/harding/aces2.par
-CFOUR=home/milias/Work/qch/software/cfour
+CFOUR=/home/milias/Work/qch/software/cfour
 QUESYS=$CFOUR/share/quesys.sh
 
 if [ -f $QUESYS ]; then
